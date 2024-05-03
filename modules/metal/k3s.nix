3b8f7d2d24412429a36ce821e0e4d96f83ac0110
Blake Hamm <blake.j.hamm@gmail.com>
2024-05-03 08:53:05 -0600

    Get working k3s with argo and update todo

--- modules/metal/k3s.nix
+++ modules/metal/k3s.nix
@@ -1,5 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
+  systemd.tmpfiles.rules = [
+    "C+ /var/lib/rancher/k3s/server/manifests/custom 0755 root root - /home/${username}/repos/k3s-config/manifests"
+  ];
   networking.firewall.allowedTCPPorts = [
     6443
   ];
@@ -9,10 +12,13 @@
     extraFlags = toString [
       "--debug"
       "--disable=traefik"
+      "--disable=servicellb"
       "--tls-san=192.168.69.20"
     ];
   };
   environment.systemPackages = [
     pkgs.k3s
+    pkgs.argo
+    pkgs.argocd
   ];
 }

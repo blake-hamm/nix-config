0f3e14ed6b53458d26baf46cd9e80db77fd2827f
Blake Hamm <blake.j.hamm@gmail.com>
2024-05-09 19:15:33 -0600

    Setup iso image

--- flake.nix
+++ flake.nix
@@ -28,12 +28,13 @@
   outputs = { nixpkgs, self, ... } @ inputs:
     let
       username = "bhamm";
+      system = "x86_64-linux";
     in
     {
       colmena = {
         meta = {
           nixpkgs = import nixpkgs {
-            system = "x86_64-linux";
+            inherit system;
           };
           specialArgs = {
             inherit self inputs username;
@@ -54,39 +55,39 @@
           imports = [ ./hosts/framework ];
         };
       };
-      # nixosConfigurations = {
+      nixosConfigurations = {
 
-      #   framework = lib.nixosSystem {
-      #     inherit system;
-      #     modules = [ (import ./hosts/framework) ];
-      #     specialArgs = {
-      #       host = "framework";
-      #       inherit self inputs username;
-      #     };
-      #   };
+        #   framework = lib.nixosSystem {
+        #     inherit system;
+        #     modules = [ (import ./hosts/framework) ];
+        #     specialArgs = {
+        #       host = "framework";
+        #       inherit self inputs username;
+        #     };
+        #   };
 
-      #   framework-vm-k3s-server-1 = lib.nixosSystem {
-      #     inherit system;
-      #     modules = [ (import ./hosts/k3s-server) ];
-      #     specialArgs = {
-      #       host_ssh_port = 14185;
-      #       host = "framework-vm-k3s-server-1";
-      #       inherit self inputs username;
-      #     };
-      #   };
+        #   framework-vm-k3s-server-1 = lib.nixosSystem {
+        #     inherit system;
+        #     modules = [ (import ./hosts/k3s-server) ];
+        #     specialArgs = {
+        #       host_ssh_port = 14185;
+        #       host = "framework-vm-k3s-server-1";
+        #       inherit self inputs username;
+        #     };
+        #   };
 
-      #   minimal-iso = lib.nixosSystem {
-      #     inherit system;
-      #     modules = [
-      #       "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-      #       (import ./hosts/iso)
-      #     ];
-      #     specialArgs = {
-      #       host = "minimal-iso";
-      #       inherit self inputs username;
-      #     };
-      #   };
+        minimal-iso = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+            (import ./hosts/iso)
+          ];
+          specialArgs = {
+            host = "minimal-iso";
+            inherit self inputs username;
+          };
+        };
 
-      # };
+      };
     };
 }

3219945c52df9d6a2055f64e902fd1363c58beb6
Blake Hamm <blake.j.hamm@gmail.com>
2024-05-12 14:06:41 -0600

    Remove state directory (not working)

--- hosts/aorus/vms.nix
+++ hosts/aorus/vms.nix
@@ -3,7 +3,11 @@
   imports = [
     inputs.microvm.nixosModules.host
   ];
-  microvm.stateDir = "/mnt/zpool_ssd/aorus/microvms";
+  # TODO: Figure out how to run vm on zfs
+  # fileSystems."/var/lib/microvms" = {
+  #   device = "/mnt/zpool_ssd";
+  #   options = [ "bind" ];
+  # };
   microvm.autostart = [
     "aorus-k3s-server-1"
   ];

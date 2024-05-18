6054d7ef39644113d0e3eb5b16a4ff671d420cc4
Blake Hamm <blake.j.hamm@gmail.com>
2024-05-18 14:43:43 -0600

    Give k3s vm 16gb ram

--- modules/k3s/microvm.nix
+++ modules/k3s/microvm.nix
@@ -9,13 +9,13 @@
 
   swapDevices = [{
     device = "/var/lib/swapfile";
-    size = 8 * 1024;
+    size = 16 * 1024;
   }];
 
   # Microvm
   microvm = {
     vcpu = 4;
-    mem = 8 * 1024; # 8gb
+    mem = 16 * 1024; # 8gb
 
     volumes = [{
       mountPoint = "/var";

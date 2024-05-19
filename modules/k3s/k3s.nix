49ff12a60b3041dfb9a66abd7a505725275048c6
Blake Hamm <blake.j.hamm@gmail.com>
2024-05-19 07:42:04 -0600

    Add logic so first node doesn't try to connect to itself

--- modules/k3s/k3s.nix
+++ modules/k3s/k3s.nix
@@ -14,8 +14,8 @@
       "--tls-san=${kube_vip}" # Also configured in kube-vip
       "--cluster-cidr=10.42.0.0/16" # Also configured in calico.yaml
     ];
-    token = if vm_name = "k3s-server-1" then "" else "my_token";
-    serverAddr = if vm_name = "k3s-server-1" then "" else "192.168.69.31:6443";
+    token = if (vm_name == "k3s-server-1") then "" else "my_token";
+    serverAddr = if (vm_name == "k3s-server-1") then "" else "192.168.69.31:6443";
   };
   environment.systemPackages = with pkgs; [
     k3s

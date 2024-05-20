a1a0e5acba88b5ee3c9a4cabe8444c19056e5894
Blake Hamm <blake.j.hamm@gmail.com>
2024-05-20 07:50:44 -0600

    Fix misspelling

--- hosts/precision/default.nix
+++ hosts/precision/default.nix
@@ -3,7 +3,7 @@
     ./hardware-configuration.nix
     ./../../modules/profiles/minimal.nix
     ./disko.nix
-    ./../../modules/hadware/nfs_client.nix
+    ./../../modules/hardware/nfs_client.nix
     ./network.nix
     ./vms.nix
   ];

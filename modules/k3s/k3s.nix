3e91aa94e8cab2d7c4969dff49f7b7ebb0d81087
Blake Hamm <blake.j.hamm@gmail.com>
2024-05-25 17:45:15 -0600

    Ensure kube-proxy strict arp

--- modules/k3s/k3s.nix
+++ modules/k3s/k3s.nix
@@ -15,6 +15,7 @@
           "--disable-network-policy"
           "--tls-san=${kube_vip}" # Also configured in kube-vip
           "--cluster-cidr=10.42.0.0/16" # Also configured in calico.yaml
+          "--kube-proxy-arg=ipvs-strict-arp=true" # Required for metallb
         ] else "";
     token = if (vm_name == "k3s-server-1") then "" else "my_token";
     serverAddr = if (vm_name == "k3s-server-1") then "" else "https://${kube_vip}:6443";

99c223633f72d5327ed3d49f4d9298864f8351c3
Blake Hamm <blake.j.hamm@gmail.com>
2024-05-25 11:04:17 -0600

    No extra flags for agent

--- modules/k3s/k3s.nix
+++ modules/k3s/k3s.nix
@@ -5,15 +5,17 @@
     enable = true;
     clusterInit = if k3s_role == "server" then true else false;
     role = "${k3s_role}";
-    extraFlags = toString [
-      "--debug"
-      "--disable=traefik"
-      "--disable=servicellb"
-      "--flannel-backend=none"
-      "--disable-network-policy"
-      "--tls-san=${kube_vip}" # Also configured in kube-vip
-      "--cluster-cidr=10.42.0.0/16" # Also configured in calico.yaml
-    ];
+    extraFlags =
+      if k3s_role == "server" then
+        toString [
+          "--debug"
+          "--disable=traefik"
+          "--disable=servicellb"
+          "--flannel-backend=none"
+          "--disable-network-policy"
+          "--tls-san=${kube_vip}" # Also configured in kube-vip
+          "--cluster-cidr=10.42.0.0/16" # Also configured in calico.yaml
+        ] else "";
     token = if (vm_name == "k3s-server-1") then "" else "my_token";
     serverAddr = if (vm_name == "k3s-server-1") then "" else "https://${kube_vip}:6443";
   };

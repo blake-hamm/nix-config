0f93c3278c3d8f1d39dd85087b240a75552c46ba
Blake Hamm <blake.j.hamm@gmail.com>
2024-07-11 19:54:19 -0600

    Update mtu

--- modules/k3s/network.nix
+++ modules/k3s/network.nix
@@ -7,6 +7,7 @@
     firewall.enable = lib.mkForce false; # Must be disabled for calico
     useDHCP = lib.mkForce false;
     enableIPv6 = lib.mkForce false;
+    interfaces.enp0s4.mtu = 1500;
   };
   systemd.network.enable = true;
   systemd.network.networks."10-lan" = {

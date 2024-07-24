c074e555cb7f0422b05d90a34d17861f1e2b7674
Blake Hamm <blake.j.hamm@gmail.com>
2024-07-23 18:34:33 -0600

    Address laptop charger script and systemd service

--- modules/hardware/laptop-server.nix
+++ modules/hardware/laptop-server.nix
@@ -4,15 +4,6 @@
 
   # Laptop charger poetry2nix app in packages/laptop-charger
   systemd = {
-    timers."manage_charger" = {
-      timerConfig = {
-        OnBootSec = "5s";
-        OnUnitActiveSec = "30m";
-        Persistent = true;
-        Unit = "manage_charger.service";
-      };
-      after = [ "timers.target" ];
-    };
     services."manage_charger" = {
       path = with pkgs; [
         inputs.manage_charger.packages."${system}".manage_charger
@@ -22,7 +13,18 @@
       '';
       serviceConfig = {
         Type = "oneshot";
+        Restart = "on-failure";
+        RestartSec = 60;
       };
+    };
+    timers."manage_charger" = {
+      timerConfig = {
+        OnBootSec = "5s";
+        OnUnitActiveSec = "30m";
+        Persistent = true;
+        Unit = "manage_charger.service";
+      };
+      wantedBy = [ "timers.target" ];
     };
   };
 }
